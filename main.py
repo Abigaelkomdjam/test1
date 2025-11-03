ccfea450b8f7b2cf0d722981116697af7992d6fd
Abigaelkomdjam <abigaelkomdjam@gmail.com>
2025-11-03 07:54:19 +0100

    test: erreurs mypy + pytest pour validation finale

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from ui.interface import lancer_interface
 
 if __name__ == "__main__":
     lancer_interface()
+# Erreur mypy
+bad: str = 123
+
+# Fonction pour test
+def fail():
+    assert False
+
