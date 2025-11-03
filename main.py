d65aa22cc1620118e33913ebfcd263e3900f536e
Abigaelkomdjam <abigaelkomdjam@gmail.com>
2025-11-03 08:30:11 +0100

    test: clean code

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-# main.py
-
-from ui.interface import lancer_interface
-
-if __name__ == "__main__":
-    lancer_interface()
-# Erreur mypy
-bad: str = 123
-
-# Fonction pour test
-def fail():
-    assert False
-
-bad: str = 123
+print('Hello')
